0b55d149d9e3f1fb54d42113d403a39826d38e81
James Hatfield <jhatfield209@gmail.com>
2020-08-15 12:27:17 -0700

    added sass structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import classes from "./App.module.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Homes from "./components/Homes/Homes";
+import Realtors from "./components/Realtors/Realtors";
+import Story from "./components/Story/Story";
+import Features from "./components/Features/Features";
+import Gallery from "./components/Gallery/Gallery";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.Container}>
+      <Header />
+      <Sidebar />
+      <Realtors />
+      <Features />
+      <Story />
+      <Homes />
+      <Gallery />
+      <Footer />
     </div>
   );
 }
