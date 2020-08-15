e95a1bea805a96cee1740bf502d790a940a5e388
James Hatfield <jhatfield209@gmail.com>
2020-08-15 15:07:49 -0700

    finished header/realtor components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,33 @@
 import React from "react";
 import classes from "./Header.module.scss";
 
-const Header = () => <header className={classes.Header}>Header</header>;
+const Header = () => (
+  <header className={classes.Header}>
+    <div className={classes.NexterLogo}>
+      <img src={require("../../assets/logo.png")} alt="Logo" />
+    </div>
+    <div className={classes.Heading}>
+      <h3>Your own home:</h3>
+      <h2>The ultimate personal freedom</h2>
+      <button>View our properties</button>
+    </div>
+    <div className={classes.Companies}>
+      <span className={classes.Line}></span>
+      <span>As seen on</span>
+      <span className={classes.Line}></span>
+      <div className={classes.ImageContainer}>
+        <img src={require("../../assets/logo-bbc.png")} alt="BBC logo" />
+        <img src={require("../../assets/logo-forbes.png")} alt="Forbes logo" />
+        <img
+          src={require("../../assets/logo-techcrunch.png")}
+          alt="Tech Crunch logo"
+        />
+        <img
+          src={require("../../assets/logo-bi.png")}
+          alt="Business Insider logo"
+        />
+      </div>
+    </div>
+  </header>
+);
 export default Header;
--- src/components/Realtors/Realtors.tsx
+++ src/components/Realtors/Realtors.tsx
@@ -1,5 +1,32 @@
 import React from "react";
 import classes from "./Realtors.module.scss";
 
-const Realtors = () => <section className={classes.Realtors}>Realtors</section>;
+const Realtors = () => (
+  <section className={classes.Realtors}>
+    <h3>Top 3 realtors</h3>
+    <div className={classes.RealtorContainer}>
+      <div className={classes.Realtor}>
+        <img src={require("../../assets/realtor-1.jpeg")} alt="Realtor 1" />
+        <div className={classes.Text}>
+          <p>Erik Feinman</p>
+          <span>245 house sold</span>
+        </div>
+      </div>
+      <div className={classes.Realtor}>
+        <img src={require("../../assets/realtor-2.jpeg")} alt="Realtor 2" />
+        <div className={classes.Text}>
+          <p>Kim Brown</p>
+          <span>212 house sold</span>
+        </div>
+      </div>
+      <div className={classes.Realtor}>
+        <img src={require("../../assets/realtor-3.jpeg")} alt="Realtor 3" />
+        <div className={classes.Text}>
+          <p>Toby Ramsey</p>
+          <span>198 house sold</span>
+        </div>
+      </div>
+    </div>
+  </section>
+);
 export default Realtors;
